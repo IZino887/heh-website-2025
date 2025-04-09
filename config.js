72347e90e60b84e160d50a1487171c4b99d79755
IZino887 <markxavierbautista4@gmail.com>
2025-04-09 14:47:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dsx7xplak/video/upload/v1744170766/Pag-ibig_ng_Ikaw_at_Ako_evn3oh.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dsx7xplak/video/upload/v1744181109/thuy_-_universe_official_visualizerlyric_video_trgaat.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
